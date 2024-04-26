376f66f6535cca1b3e2c77e0e5052a3245833e85
Michelle <nyamburamichelle400@gmail.com>
2024-04-26 16:11:09 +0300

    completes lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,28 @@
 import React from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import { useState } from "react";
 
 function App() {
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [isDarkMode, setIsDarkMode] = useState(false);
+ // the false indicates that initial mode is not darkmode...
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+
+  const appClass = isDarkMode ? "App dark" : "App light"
+  //if is dark mode, run appdark, else run applight
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={toggleDarkMode}>
+          {isDarkMode ? "Light Mode" : "Dark Mode"}
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { useState } from "react";
+
 
 function Item({ name, category }) {
+    const[isInCart, setIsInCart] =useState(false)
+
+//this function notes on when adding or removing from cart
+    const toggleCartStatus = () => {
+      setIsInCart(!isInCart);
+    };
+
   return (
-    <li className="">
+<li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={toggleCartStatus}>
+        {isInCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
